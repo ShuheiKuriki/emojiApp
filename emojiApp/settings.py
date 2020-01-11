ca2bb96b6cd0e9476e0b92eec896ef237f279c71
ShuheiKuriki <shukuri.7336.8@gmail.com>
2020-01-11 21:45:05 +0900

    edit static settings

--- emojiApp/settings.py
+++ emojiApp/settings.py
@@ -109,12 +109,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-
-<<<<<<< HEAD
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-=======
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
->>>>>>> 3fd8ab6979beaad5f869a251a6b72ce97352a31a
 
 
 try:
